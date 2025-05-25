a2d6ce19e8506af43ef18670244e330b179cb6a9
Sanchay Sahay <grindinforvalore@gmail.com>
2025-05-25 23:18:53 +0530

    string pallindrome checker

---
+++ basic/pallindromeString.cpp
@@ -0,0 +1,25 @@
+// Given a string s, return true if the string is palindrome, otherwise false.
+
+#include <iostream>
+#include <string>
+#include <string.h>
+
+bool pallindromeCheck1(std::string& s)
+{
+    //for loop method
+    for(int i=0; i<s.size();i++)
+    {
+        if(s[i]!=s[s.size()-i-1])
+        return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    std::string inp;
+    std::cin>>inp;
+
+    std::cout<<pallindromeCheck1(inp);
+}
